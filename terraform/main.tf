04d495bea89d9090cb7fbc9d8795909f417254d3
Kavallerya <illya.morozow.1998@gmail.com>
2026-05-16 13:11:46 +0200

    disable GKE autopilot

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,27 @@
-# Створюємо ізольовану мережу (VPC)
 resource "google_compute_network" "vpc_network" {
   name                    = "cv-k8s-network"
   auto_create_subnetworks = true
 }
 
-# Створюємо GKE Autopilot кластер
 resource "google_container_cluster" "primary" {
   name     = "cv-cluster"
-  location = var.region
+  location = "europe-central2"
+  network  = google_compute_network.vpc_network.name
 
-  # Вмикаємо режим Autopilot (Google сам керує нодами)
-  enable_autopilot = true
-  
-  network = google_compute_network.vpc_network.name
+  # 1. ВИМИКАЄМО AUTOPILOT
+  enable_autopilot = false
 
-  # Дозволяємо Терраформу видаляти кластер (без цього terraform destroy видасть помилку)
-  deletion_protection = false 
+  # 2. НАЛАШТОВУЄМО ФІКСОВАНИЙ КЛАСТЕР (GKE Standard)
+  initial_node_count = 2 # Створити рівно 2 сервери
+
+  node_config {
+    machine_type = "e2-standard-2" # Кожен сервер: 2 vCPU, 8GB RAM. (Разом 4 vCPU — ідеально для квот!)
+    
+    # Обов'язкові права для нод, щоб вони могли спілкуватися з API Google
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  deletion_protection = false
 }
